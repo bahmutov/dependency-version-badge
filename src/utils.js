ec7b7e2a79f1b1b2713d50bb537dda842c4a2c62
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-05-09 08:02:54 -0400

    refactor

---
+++ src/utils.js
@@ -0,0 +1,59 @@
+// @ts-check
+const debug = require('debug')('dependency-version-badge')
+const path = require('path')
+const fs = require('fs')
+
+function updateBadge(name) {
+  debug('updating badge "%s"', name)
+  const pkgFilename = path.join(process.cwd(), 'package.json')
+  const pkg = require(pkgFilename)
+  const allDependencies = Object.assign(
+    {},
+    pkg.dependencies,
+    pkg.devDependencies,
+  )
+  const currentVersion = allDependencies[name]
+  if (!currentVersion) {
+    console.error('Could not find dependency %s among dependencies', name)
+    process.exit(1)
+  }
+  debug('current dependency version %s@%s', name, currentVersion)
+
+  const readmeFilename = path.join(process.cwd(), 'README.md')
+  const readmeText = fs.readFileSync(readmeFilename, 'utf8')
+
+  function escapeName(name) {
+    // for shields.io need to change each '-' into '--'
+    return name.replace(/-/g, '--')
+  }
+
+  function replaceVersionShield(name, newVersion) {
+    const escapedName = escapeName(name)
+    const cypressVersionRe = new RegExp(
+      'https://img\\.shields\\.io/badge/' +
+        escapedName +
+        '-(\\d+\\.\\d+\\.\\d+)-brightgreen',
+    )
+    const cypressNewVersion = `https://img.shields.io/badge/${escapedName}-${newVersion}-brightgreen`
+    debug('new badge contents "%s"', cypressNewVersion)
+
+    const updatedReadmeText = readmeText.replace(
+      cypressVersionRe,
+      cypressNewVersion,
+    )
+    return updatedReadmeText
+  }
+
+  // replaceVersionShield('cypress', '4.5.1')
+  const maybeChangedText = replaceVersionShield(name, currentVersion)
+  if (maybeChangedText !== readmeText) {
+    console.log('saving updated readme with %s@%s', name, currentVersion)
+    fs.writeFileSync(readmeFilename, maybeChangedText, 'utf8')
+  } else {
+    debug('no updates for dependency %s', name)
+  }
+}
+
+module.exports = {
+  updateBadge,
+}
