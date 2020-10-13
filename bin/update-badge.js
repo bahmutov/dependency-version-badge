7690e8d6de7416f5d179f508a91f54e5786d3db1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2020-10-13 14:57:20 -0400

    add parsing parameters with unit tests

--- bin/update-badge.js
+++ bin/update-badge.js
@@ -1,8 +1,14 @@
 #!/usr/bin/env node
 // @ts-check
 
+const arg = require('arg')
 const debug = require('debug')('dependency-version-badge')
 const { updateBadge } = require('../src/utils')
+
+const args = arg({
+  '--from': String,
+})
+debug('args: %o', args)
 
 if (process.argv.length < 3) {
   console.error('Usage: update-version <package name> <package name2> ...')
---
+++ test/args.test.js
@@ -0,0 +1,40 @@
+const test = require('ava')
+const arg = require('arg')
+const debug = require('debug')('test')
+
+const argOptions = {
+  '--from': String,
+}
+test('parsing empty list', (t) => {
+  const parsed = arg(argOptions, { argv: [] })
+  debug('parsed %o', parsed)
+  t.deepEqual(parsed, {
+    _: [],
+  })
+})
+
+test('parsing a few names', (t) => {
+  const parsed = arg(argOptions, { argv: ['a', 'b'] })
+  debug('parsed %o', parsed)
+  t.deepEqual(parsed, {
+    _: ['a', 'b'],
+  })
+})
+
+test('parsing a few names followed by --from', (t) => {
+  const parsed = arg(argOptions, { argv: ['a', 'b', '--from', 'foo'] })
+  debug('parsed %o', parsed)
+  t.deepEqual(parsed, {
+    '--from': 'foo',
+    _: ['a', 'b'],
+  })
+})
+
+test('parsing --from then a few names', (t) => {
+  const parsed = arg(argOptions, { argv: ['--from', 'foo', 'a', 'b'] })
+  debug('parsed %o', parsed)
+  t.deepEqual(parsed, {
+    '--from': 'foo',
+    _: ['a', 'b'],
+  })
+})
